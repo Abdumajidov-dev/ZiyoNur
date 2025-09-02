1ca4e9873d4b93c9c8d72e09129c0cd97f07e1ae
Abdumajidov Dev <abdumajidova51@gmail.com>
2025-09-02 10:37:41 +0500

    service leir  arxitektuasi o'zgardi

--- ZiyoNur/ZiyoNur.Service/Services/Interfaces/ISmsService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ZiyoNur.Service.Services.Interfaces;
-
-public interface ISmsService
-{
-    Task<bool> SendSmsAsync(string phoneNumber, string message);
-    Task<bool> SendBulkSmsAsync(List<string> phoneNumbers, string message);
-    Task<bool> SendOtpAsync(string phoneNumber, string code);
-    Task<bool> SendOrderNotificationAsync(string phoneNumber, string orderNumber);
-}
